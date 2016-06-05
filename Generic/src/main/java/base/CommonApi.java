aaae0c4803eb2c235965f54709039cde6e0f4a01
iqbal <iqbalchoudhury2@gmail.com>
2016-06-05 18:15:59 -0500

    Add sauceLab

--- Generic/src/main/java/base/CommonApi.java
+++ Generic/src/main/java/base/CommonApi.java
@@ -9,7 +9,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Optional;
+import java.io.IOException;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -17,23 +24,57 @@ import java.util.concurrent.TimeUnit;
 public class CommonAPI {
 
     public WebDriver driver = null;
-    @Parameters({"url"})
+
+    @Parameters({"usecloud","userName","accessKey","os","browserName","browserVersion","url"})
     @BeforeMethod
-    public void setUp(String url) {
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void setUp(@Optional("false") boolean usecloud,@Optional("") String userName,@Optional("")
+            String accessKey, @Optional("Windows 8") String os,@Optional("firefox") String browserName,@Optional("34")
+                              String browserVersion,@Optional("http://www.cnn.com") String url)throws IOException {
+        if(usecloud==true){
+            //run in cloud
+            getCloudDriver(userName,accessKey,os,browserName,browserVersion);
+        }else{
+            //run in local
+            getLocalDriver(browserName);
+        }
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get(url);
         driver.manage().window().maximize();
     }
 
-    @AfterMethod
-    public void cleanUp() {
-        driver.close();
-    }
-    public void clickById(String locator){
-        driver.findElement(By.id(locator)).click();
+    public WebDriver getLocalDriver(String browserName){
+        if(browserName.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver","Generic/browser-driver/chromedriver.exe");
+            driver = new ChromeDriver();
+        }else if(browserName.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        } else if(browserName.equalsIgnoreCase("ie")) {
+            System.setProperty("webdriver.ie.driver", "Generic/browser-driver/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+        }
+        return driver;
+
     }
 
+    public WebDriver getCloudDriver(String userName,String accessKey,String os, String browserName,
+                                    String browserVersion)throws IOException {{
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("platform", os);
+        cap.setBrowserName(browserName);
+        cap.setCapability("version",browserVersion);
+        driver = new RemoteWebDriver(new URL("http://"+userName+":"+accessKey+
+                "@ondemand.saucelabs.com:80/wd/hub"), cap);
+
+        return driver;
+    }
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.quit();
+    }
 
     public void clickByCss(String locator) {
         driver.findElement(By.cssSelector(locator)).click();
@@ -54,9 +95,7 @@ public class CommonAPI {
     public void takeEnterKeys(String locator) {
         driver.findElement(By.cssSelector(locator)).sendKeys(Keys.ENTER);
     }
-public void typeById(String locator,String value){
-    driver.findElement(By.id(locator)).sendKeys(value);
-}
+
     public List<WebElement> getListOfWebElements(String locator) {
 
         List<WebElement> list = new ArrayList<WebElement>();
@@ -74,13 +113,14 @@ public void typeById(String locator,String value){
 
         return items;
     }
+    public void clickById(String locator){
+        driver.findElement(By.id(locator)).click();}
 
+    public void typeById(String locator,String value){
+        driver.findElement(By.id(locator)).sendKeys(value);
+    }
     public void selectOptionByVisibleText(WebElement element, String value) {
         Select select = new Select(element);
         select.selectByVisibleText(value);
     }
-
-
-
-
 }
